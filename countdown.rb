0281b0d1122a70018a6946008d2c2e41f3be07bf
Andrew Kairys <andrewkairys@gmail.com>
2018-03-05 13:14:58 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,8 @@ integer = 0
 
 def countdown(integer)
   while integer > 0
-    integer = integer -= 1
     puts "#{integer} SECOND(S)!"
+    integer -= 1
   break integer == 0
   puts "HAPPY NEW YEAR!"
  end
